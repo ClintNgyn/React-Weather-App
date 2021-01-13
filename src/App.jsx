58020620f3f305608fe720de0e0c27b6124f77eb
Clinton Ng <nguyennoelclinton1@gmail.com>
2021-01-13 08:07:50 -0500

    search box animation, location and temperature ui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,61 @@
 const { REACT_APP_API_KEY: key } = process.env;
 const base_url = 'https://api.openweathermap.org/data/2.5/weather';
 
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
 const App = () => {
+  const dateBuilder = (d) => {
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}, ${month} ${date}, ${year}`;
+  };
+
   return (
     <>
-      <div className='bg'></div>
       <div className='app'>
         <main>
           <div className='search-box'>
             <input
               type='text'
               className='search-bar'
-              placeholder='Enter city'
+              placeholder='Enter City'
             />
             <button className='btn'>Search</button>
+          </div>
+          <div className='location-box'>
+            <div className='location'>London, UK</div>
+            <div className='date'>{dateBuilder(new Date())}</div>
+          </div>
+          <div className='weather-box'>
+            <div className='temperature'>
+              35°F
+              <div className='condition'>Cloudy</div>
+            </div>
           </div>
         </main>
       </div>
