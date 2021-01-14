83f14b622480ca7196e6af4d2cb65bd0ed0865f0
Clinton Ng <nguyennoelclinton1@gmail.com>
2021-01-14 15:29:41 -0500

    updated image assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,49 @@
 import { useEffect, useState } from 'react';
 import moment from 'moment';
 
-const { REACT_APP_API_KEY: key } = process.env;
-
-const url = 'https://api.openweathermap.org/data/2.5/weather';
-
-const months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  ' July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
-
-const days = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-];
+const { REACT_APP_API_KEY: key, REACT_APP_API_BASE_URL: url } = process.env;
 
 const App = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  // const [searchQuery, setSearchQuery] = useState('');
   const [data, setData] = useState();
   const [degree, setDegree] = useState('°C');
 
   useEffect(() => {
     (async () => {
-      // const response = await fetch(`${url}?appid=${key}&q=Montreal`);
-      // setData(await response.json());
+      const response = await fetch(
+        `${url}/data/2.5/weather?appid=${key}&q=Montreal`
+      );
+      setData(await response.json());
     })();
   }, []);
+
+  const getWeatherCondition = () => {
+    const {
+      weather: [{ description }],
+      sys: { sunset },
+      dt,
+    } = data;
+
+    if (description.includes('rain')) {
+      return 'rain';
+    }
+
+    if (dt + 3600 >= sunset) {
+      return 'night';
+    }
+
+    return '';
+  };
 
   const dateBuilder = () => {
     return moment(Date.now()).format('dddd, MMMM Do, YYYY');
   };
 
   const toCelsius = (kelvin) => {
-    // C = K - 273
     return Math.round(kelvin - 273);
   };
 
   const toFahrenheit = (kelvin) => {
-    // F = 1.8(K - 273) + 32
     return Math.round(1.8 * (kelvin - 273) + 32);
   };
 
@@ -62,7 +53,11 @@ const App = () => {
 
   return (
     <>
-      <div className='app'>
+      <div
+        className={
+          typeof data === 'undefined' ? '' : 'app' + getWeatherCondition()
+        }
+      >
         <main>
           <div className='search-box'>
             <input
@@ -75,17 +70,20 @@ const App = () => {
           </div>
 
           <div className='location-box'>
-            <div className='location'>London, UK</div>
+            <div className='location'>
+              {data?.name}, {data?.sys?.country}
+            </div>
+
             <div className='date'>{dateBuilder()}</div>
           </div>
 
           <div className='weather-box'>
             <div className='temperature' onClick={changeDegreeHandler}>
               {(degree === '°C'
-                ? toCelsius(data?.main?.feel_like)
-                : toFahrenheit(data?.main?.feel_like)) + degree}
+                ? toCelsius(data?.main?.temp)
+                : toFahrenheit(data?.main?.temp)) + degree}
 
-              <div className='condition'>Cloudy</div>
+              <div className='condition'>{data?.weather[0]?.description}</div>
             </div>
           </div>
         </main>
