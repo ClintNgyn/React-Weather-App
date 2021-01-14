79d601fead8c74003429f4cfbbeac049c650546f
Clinton Ng <nguyennoelclinton1@gmail.com>
2021-01-13 21:45:06 -0500

    fetch api working

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,31 @@
-const { REACT_APP_API_KEY: key } = process.env;
-const base_url = 'https://api.openweathermap.org/data/2.5/weather';
+import { useEffect, useState } from 'react';
 
-const months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
-const days = [
-  'Sunday',
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-];
+const {
+  REACT_APP_API_KEY: key,
+  REACT_APP_BASE_URL: url,
+  REACT_APP_MONTHS: months,
+  REACT_APP_DAYS: days,
+} = process.env;
 
 const App = () => {
-  const dateBuilder = (d) => {
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+  const [searchQuery, setSearchQuery] = useState('');
+  const [data, setData] = useState({});
 
-    return `${day}, ${month} ${date}, ${year}`;
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`${url}?appid=${key}&q=Montreal`);
+      setData(await response.json());
+    })();
+  }, []);
+
+  const dateBuilder = () => {
+    const d = new Date();
+    return `${d.getDay()}, ${d.getMonth()} ${d.getDate()}, ${d.getFullYear()}`;
   };
 
   return (
     <>
+      {console.log(data)}
       <div className='app'>
         <main>
           <div className='search-box'>
@@ -47,10 +36,12 @@ const App = () => {
             />
             <button className='btn'>Search</button>
           </div>
+
           <div className='location-box'>
             <div className='location'>London, UK</div>
-            <div className='date'>{dateBuilder(new Date())}</div>
+            <div className='date'>{dateBuilder()}</div>
           </div>
+
           <div className='weather-box'>
             <div className='temperature'>
               35°F
